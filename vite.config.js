5464f31729ba9c21f6f75868388a546d04d96107
Joao Nascimento <nascijoao85_jua@hotmail.com>
2026-08-14 23:31:26 -0300

    Segundo ajuste para corrigir limite chunk

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          recharts: ['recharts'],
+        manualChunks(id) {
+          if (id.includes('node_modules/recharts')) {
+            return 'recharts';
+          }
+          if (id.includes('node_modules/react')) {
+            return 'vendor';
+          }
         },
       },
     },
